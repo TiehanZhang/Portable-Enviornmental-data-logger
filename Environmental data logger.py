3b7c05ba221e5f633efa1f800035693ba851966c
TiehanZhang <47127787+TiehanZhang@users.noreply.github.com>
2020-02-25 12:16:12 -0500

    Update Environmental data logger.py

--- Environmental data logger.py
+++ Environmental data logger.py
@@ -7,8 +7,8 @@ mt.compass.calibrate()
 
 while True:
     with open('data_logger.csv') as tiehan_file:
-        content = my_file.read()
+        content = tiehan_file.read()
     content = content + str(mt.temperature()) + ", " + str(mt.compass.heading()) + "\n"
     with open('data_logger.csv', "w") as tiehan_file:
-        my_file.write(content)
+        tiehan_file.write(content)
     sleep(5000)
