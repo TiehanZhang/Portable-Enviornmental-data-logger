a3cbee5690ccb4229878ca47a13a33cd677a008d
TiehanZhang <47127787+TiehanZhang@users.noreply.github.com>
2020-02-25 12:34:24 -0500

    Update Environmental data logger.py

--- Environmental data logger.py
+++ Environmental data logger.py
@@ -1,10 +1,10 @@
 # Write your code here :-)
-import microbit as mt
+import microbit as zth
 
 with open('data_logger.csv', "w") as tiehan_file:
     my_file.write("")
-mt.compass.calibrate()
-
+zth.compass.calibrate()
+# default initiating for the write mode of my file
 while True:
     with open('data_logger.csv') as tiehan_file:
         content = tiehan_file.read()
