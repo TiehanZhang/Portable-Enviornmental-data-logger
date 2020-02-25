5ad1940a979efc35b0a47a4e10d106a0bb8dba6d
TiehanZhang <47127787+TiehanZhang@users.noreply.github.com>
2020-02-25 13:12:50 -0500

    Update Environmental data logger.py

--- Environmental data logger.py
+++ Environmental data logger.py
@@ -1,13 +1,16 @@
-# Write your code here :-)
 from microbit import *
-with open('data_logger.csv', "w") as tiehan_file:
-    tiehan_file.write("")
-microbit.compass.calibrate()
+with open('data_logger.csv', "w") as my_file:
+    my_file.write("Hello")
+compass.calibrate()
+turn = 0
 # default initiating for the write mode of my file
 while True:
-    with open('data_logger.csv') as tiehan_file:
-        content = tiehan_file.read()
+    with open('data_logger.csv') as my_file:
+        content = my_file.read()
     content = content + str(temperature()) + ", " + str(compass.heading()) + "\n"
-    with open('data_logger.csv', "w") as tiehan_file:
-        tiehan_file.write(content)
+    with open('data_logger.csv', "w") as my_file:
+        my_file.write(content)
+    display.show(turn)
+    turn = turn + 1
+    # count the turns of uploading the data
     sleep(10000)
